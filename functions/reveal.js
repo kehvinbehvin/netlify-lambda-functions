c9e55e3120bbaa70a2d16a35f499cc32027424e6
Kevin Ho <hokevin78@gmail.com>
2022-03-30 01:25:03 +0800

    feature: add logs for debugging

--- functions/reveal.js
+++ functions/reveal.js
@@ -7,14 +7,24 @@ const fs = require("fs");
 const { NODE_PROVIDER_API_URL, SIGNER_PRIVATE_KEY, WALLET_ADDRESS, NFT_METADATA_CONTRACT_ADDRESS} = process.env;
 
 exports.handler = async function(event, context) {
+    console.log("NODE_PROVIDER_API_URL",NODE_PROVIDER_API_URL)
+    console.log("SIGNER_PRIVATE_KEY",SIGNER_PRIVATE_KEY)
+    console.log("WALLET_ADDRESS",WALLET_ADDRESS)
+    console.log("NFT_METADATA_CONTRACT_ADDRESS",NFT_METADATA_CONTRACT_ADDRESS)
     const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
     const web3 = createAlchemyWeb3(NODE_PROVIDER_API_URL);
+    console.log("web3 initiated")
     const myAddress = WALLET_ADDRESS
     const nonce = await web3.eth.getTransactionCount(myAddress, 'latest');
+    console.log("nonce retrieved")
     const contractAbiRaw = fs.readFileSync(path.resolve(__dirname,"../contracts/MissionDAOMetaData.example.json"));
+    console.log("contract abi retrieved")
     const contractAbi = JSON.parse(contractAbiRaw.toString());
+    console.log("contract abi parsed")
     const contractInstance = await new web3.eth.Contract(contractAbi.abi)
+    console.log("contract instantiated")
     const functionData = contractInstance.methods.reveal(16).encodeABI();
+    console.log("function encoded")
 
     const transaction = {
         'to': NFT_METADATA_CONTRACT_ADDRESS,
@@ -24,11 +34,13 @@ exports.handler = async function(event, context) {
         'nonce': nonce,
         'data': functionData,
     };
+    console.log("transaction created:", transaction)
 
     const signedTx = await web3.eth.accounts.signTransaction(transaction, SIGNER_PRIVATE_KEY);
-
+    console.log("transaction signed", signedTx)
     try {
         const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        console.log("transaction sent and is successful")
         return {
             statusCode: 200,
             body: JSON.stringify({message: "Success!!"})
