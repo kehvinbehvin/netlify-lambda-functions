40e20fc3032cbeb0b04f33448bf037b1eb891783
Kevin Ho <hokevin78@gmail.com>
2022-03-30 00:50:36 +0800

    build: initialise project and install dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("No Reveal")
+
+  const fetchData = async () => {
+    const results = await axios.get("/.netlify/functions/reveal");
+    setMessage(results.data.message)
+  }
+
+  const reveal = () => {
+    fetchData()
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +22,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{ message }</p>
+        <btn onClick={reveal}>Reveal</btn>
       </header>
     </div>
   );
